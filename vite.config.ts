686849cf1c4237f118328660bb339fd7d4ca5735
Dewmith Akalanka <akalankadewmith@gmail.com>
2025-02-23 23:47:47 +0530

    allowed hosts in preview mode

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  preview: {
+    allowedHosts: ["lms-app-frontend-production.up.railway.app"],
+  },
 });
