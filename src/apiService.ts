def1ac0f239d043ef30b10d2a2b0b139c7d6335b
Dewmith Akalanka <akalankadewmith@gmail.com>
2025-02-23 22:54:55 +0530

    change backend API url

--- src/apiService.ts
+++ src/apiService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const API_BASE_URL = "http://localhost:3000/";
+const API_BASE_URL = "https://lms-app-backend-production.up.railway.app/";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
